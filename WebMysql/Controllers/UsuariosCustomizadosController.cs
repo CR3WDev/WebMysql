24cd05092d75a6f8722beca138d393ab001169ac
marcelo victor <marcelo victor@DESKTOP-GIJMCM0>
2021-08-02 15:21:50 -0300

    Add project files.

---
+++ WebMysql/Controllers/UsuariosCustomizadosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMysql.Models;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+
+namespace WebMysql.Controllers
+{
+    public class UsuariosCustomizadosController : Controller
+    {
+        private readonly Contexto _context;
+
+        public UsuariosCustomizadosController(Contexto contexto)
+        {
+            _context = contexto;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            var listaRetorno = await _context.UsuarioCustomizado
+                .FromSqlRaw("CALL UsuariosCustomizados").ToListAsync();
+
+            return View(listaRetorno);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            var param = new MySqlParameter("id", id);
+            var usuario = await _context.UsuarioCustomizado
+                .FromSqlRaw("CALL ObterUsuariosCustomizados(@id)", param).ToListAsync();
+
+            if (usuario == null)
+                return NotFound();
+
+
+            return View(usuario.FirstOrDefault());
+        }
+    }
+}
---
+++ WebMysql/Models/UsuarioCustomizado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMysql.Models
+{
+    public class UsuarioCustomizado
+    {
+        [Key]
+        [Display(Name = "Código ")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome Usuário")]
+        public string NomeUsuario { get; set; }
+
+        [Display(Name = "Data Atual")]
+        public string DataAtual { get; set; }
+    }
+}
